8cde1f666642e3af41d691847dd88b2bf7f5473f
firoznep <firoz.thekhan@gmail.com>
2020-06-26 15:44:24 +0545

    Updates

---
+++ precache-manifest.b146158839b89f0b94dad12656b5bb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733beab186b80ba3f1329e66de2ed7ee",
+    "url": "/git-practice/index.html"
+  },
+  {
+    "revision": "160f18ad07959ed114c3",
+    "url": "/git-practice/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "160f18ad07959ed114c3",
+    "url": "/git-practice/static/js/2.b7a10c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-practice/static/js/2.b7a10c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482098737fa4d7ab458f",
+    "url": "/git-practice/static/js/main.259fedac.chunk.js"
+  },
+  {
+    "revision": "fe7f3ee03923135b3508",
+    "url": "/git-practice/static/js/runtime-main.eefef85d.js"
+  }
+]);
